f4c65214b098b05400f502f6fcfeb9fdd9f48521
Franco1606 <frankobertolotti@gmail.com>
2022-03-07 19:50:00 -0300

    Cree login.php

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php 
+require_once 'clases/login.class.php';
+require_once 'clases/respuestas.class.php';
+
+$_login = new login;
+$_respuestas = new respuestas;
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+    //Recibir datos
+    $postBody = file_get_contents("php://input");    
+           
+    //Envio de datos al manejador
+    $response = $_login->login($postBody);
+    
+    //Respuesta
+    header('Content-Type: application/json');
+    if(isset($response["result"]["error_id"])){
+        $responseCode = $response["result"]["error_id"];
+        http_response_code($responseCode);
+    }else{
+        http_response_code(200);
+    }
+    echo json_encode($response);    
+
+}else{
+    header('Content-Type: application/json');
+    $response = $_respuestas->error_405();
+    echo json_encode($response);
+}
+?>
