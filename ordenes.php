a2a90c72f90b46854667329573eaaa0511be1a9b
Franco1606 <frankobertolotti@gmail.com>
2022-03-12 09:56:41 -0300

    Cree el archivo ordenes.php

---
+++ ordenes.php
@@ -0,0 +1,73 @@
+<?php 
+require_once "clases/respuestas.class.php";
+require_once "clases/ordenes.class.php";
+
+$ordenes = new ordenes;
+$_respuestas = new respuestas;
+
+if($_SERVER["REQUEST_METHOD"] == "GET") {
+    if(isset($_GET["usuarioID"]) && isset($_GET["sesionID"])) {
+        $usuarioID = $_GET["usuarioID"];
+        $sesionID = $_GET["sesionID"];
+        $datosordenes = $ordenes->obtenerOrdenes($usuarioID, $sesionID);
+    } else if(isset($_GET["ordenID"])) {
+        $ordenID = $_GET["ordenID"];
+        $datosordenes = $ordenes->obtenerOrden($ordenID);
+    }
+    header("Content-Type: application/json");
+    echo json_encode($datosordenes);
+    http_response_code(200);
+
+} else if($_SERVER["REQUEST_METHOD"] == "POST") {
+    //Recibir de datos
+    $postBody = file_get_contents("php://input");    
+    //Envio de datos al manejador    
+    $respuesta = $ordenes->post($postBody);
+    //Respuesta 
+    header('Content-Type: application/json');
+    if(isset($respuesta["result"]["error_id"])){
+        $responseCode = $respuesta["result"]["error_id"];
+        http_response_code($responseCode);
+    }else {
+        http_response_code(200);
+    }
+    echo json_encode($respuesta);
+
+} else if($_SERVER['REQUEST_METHOD'] == "PUT"){
+    //Recibir de datos
+    $postBody = file_get_contents("php://input");
+    //Envio de datos al manejador
+    $respuesta = $ordenes->put($postBody);
+    //Respuesta 
+    header('Content-Type: application/json');
+    if(isset($respuesta["result"]["error_id"])){
+        $responseCode = $respuesta["result"]["error_id"];
+        http_response_code($responseCode);
+    }else{
+        http_response_code(200);
+    }
+    echo json_encode($respuesta);
+
+} else if($_SERVER["REQUEST_METHOD"] == "DELETE") {
+    //Recibir de datos
+    $postBody = file_get_contents("php://input");
+    //Envio de datos al manejador
+    $respuesta = $ordenes->delete($postBody);
+    //Respuesta
+    header('Content-Type: application/json');
+    if(isset($respuesta["result"]["error_id"])){
+        $responseCode = $respuesta["result"]["error_id"];
+        http_response_code($responseCode);
+    }else {
+        http_response_code(200);
+    }
+    echo json_encode($respuesta);    
+}
+
+else {    
+    header("Content-Type: application/json");
+    $respuesta = $_respuestas->error_405();
+    echo json_encode($respuesta);
+} 
+
+?>
