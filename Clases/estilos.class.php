346874bf6841b83a603cc886d5ecba06237531c0
Franco1606 <frankobertolotti@gmail.com>
2022-03-15 10:18:07 -0300

    Agregue metodo put

--- Clases/estilos.class.php
+++ Clases/estilos.class.php
@@ -9,6 +9,7 @@ class estilos extends conexion {
     private $usuarioID;
     private $nombre;
     private $valor;
+    private $mostrar;
     private $estilos;
 
     public function obtenerEstilos($usuarioID) {
@@ -21,11 +22,11 @@ class estilos extends conexion {
         }
     }
 
-    public function obtenerEstiloPorNombre() {
-        $query = "SELECT * FROM " . $this->tabla . " WHERE usuarioID = '" . $this->usuarioID . "' AND nombre = '" . $this->nombre . "'";        
-        $datosEstilos = parent::obtenerDatos($query);        
-        if($datosEstilos) {
-            return $datosEstilos[0];
+    public function obtenerEstiloPorNombre($usuarioID, $nombre) {
+        $query = "SELECT * FROM " . $this->tabla . " WHERE usuarioID = '" . $usuarioID . "' AND nombre = '" . $nombre . "'";  
+        $datosEstilo = parent::obtenerDatos($query);                    
+        if($datosEstilo) {
+            return $datosEstilo[0];
         } else {
             return 0;
         }
@@ -65,7 +66,8 @@ class estilos extends conexion {
         foreach($this->estilos as $estilo) {            
             $this->nombre = $estilo["nombre"];
             $this->valor = $estilo["valor"];
-            $verificarExistencia = $this->obtenerEstiloPorNombre();
+            $this->mostrar = $estilo["mostrar"];
+            $verificarExistencia = $this->obtenerEstiloPorNombre($this->usuarioID, $this->nombre);            
             if($verificarExistencia) {
                 $resp = $this->modificarEstilo();
                 if(!$resp){
@@ -82,7 +84,7 @@ class estilos extends conexion {
     }
 
     private function insertarEstilo(){
-        $query = "INSERT INTO " . $this->tabla . " (nombre, valor, usuarioID) values ('" . $this->nombre . "','" . $this->valor . "','" . $this->usuarioID . "')";         
+        $query = "INSERT INTO " . $this->tabla . " (nombre, valor, mostrar, usuarioID) values ('" . $this->nombre . "','" . $this->valor . "','" . $this->mostrar . "','" . $this->usuarioID . "')";         
         
         $resp = parent::nonQueryId($query);
         if($resp){
@@ -93,14 +95,54 @@ class estilos extends conexion {
     }    
 
     private function modificarEstilo(){
-        $query = "UPDATE " . $this->tabla . " SET nombre ='" . $this->nombre ."', valor = '" . $this->valor . "' WHERE usuarioID = '" . $this->usuarioID . "' AND nombre = '" . $this->nombre . "'";         
+        $query = "UPDATE " . $this->tabla . " SET nombre ='" . $this->nombre . "', valor = '" . $this->valor . "', mostrar = '" . $this->mostrar . "' WHERE usuarioID = '" . $this->usuarioID . "' AND nombre = '" . $this->nombre . "'";                 
         $resp = parent::nonQueryUpdate($query);       
         if($resp){
              return 1;
         }else{
             return 0;
         }
-    }    
+    }
+
+    public function put($postBody) {
+        $_respuestas = new respuestas;
+        $_token = new token;        
+        $datos = json_decode($postBody, true);
+        $verificarToken = $_token->verificarToken($datos);
+        if($verificarToken == 1){
+            if(!isset($datos["mostrar"]) || !isset($datos["nombre"]) || !isset($datos["usuarioID"])){
+                return $_respuestas->error_400();
+            } else {
+                $this->usuarioID = $datos["usuarioID"];
+                $this->mostrar = $datos["mostrar"];
+                $this->nombre = $datos["nombre"];
+                $resp = $this->modificarMostrar();                               
+                if($resp) {                    
+                    $respuesta = $_respuestas->response;
+                    $respuesta["result"] = array(
+                        "status" => "ok"                        
+                    );
+                    return $respuesta;
+                } else {
+                    return $_respuestas->error_500("Error interno del servidor, el cambio no se guardo o no hubo modificaciones en el registro");
+                }
+            }
+
+        } else {
+            return $verificarToken;
+        }
+    }
+
+    private function modificarMostrar(){
+        $query = "UPDATE " . $this->tabla . " SET mostrar ='" . $this->mostrar ."' WHERE usuarioID = '" . $this->usuarioID . "' AND nombre = '" . $this->nombre . "'";         
+        $resp = parent::nonQueryUpdate($query);       
+        if($resp >= 1){
+             return 1;
+        }else{
+            return 0;
+        }
+    }
+
 }
 
 ?>
