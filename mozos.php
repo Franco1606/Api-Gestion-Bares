e1f77773a7664839f68aa663314386c9a277bff7
Franco1606 <frankobertolotti@gmail.com>
2022-03-18 14:42:58 -0300

    Argegue operaciones crud

--- mozos.php
+++ mozos.php
@@ -8,10 +8,13 @@ $_mozos = new mozos;
 if($_SERVER['REQUEST_METHOD'] == "GET"){
     if(isset($_GET['usuarioID'])) { 
         $usuarioID = $_GET['usuarioID'];
+        $datos = $_mozos->obtenerMozos($usuarioID);
+    } else if (isset($_GET['mozoID'])) {
+        $mozoID = $_GET['mozoID'];
+        $datos = $_mozos->obtenerMozo($mozoID);
     }            
-    $datosMozos = $_mozos->obtenerMozos($usuarioID);
     header("Content-Type: application/json");
-    echo json_encode($datosMozos);
+    echo json_encode($datos);
     http_response_code(200);
        
 } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -29,6 +32,35 @@ if($_SERVER['REQUEST_METHOD'] == "GET"){
     }
     echo json_encode($respuesta);
 
+} else if($_SERVER['REQUEST_METHOD'] == "PUT"){
+    //Recibir de datos
+    $postBody = file_get_contents("php://input");
+    //Envio de datos al manejador
+    $respuesta = $_mozos->put($postBody);
+    //Respuesta 
+    header('Content-Type: application/json');
+    if(isset($respuesta["result"]["error_id"])){
+        $responseCode = $respuesta["result"]["error_id"];
+        http_response_code($responseCode);
+    }else{
+        http_response_code(200);
+    }
+    echo json_encode($respuesta);
+
+} else if($_SERVER["REQUEST_METHOD"] == "DELETE") {
+    //Recibir de datos
+    $postBody = file_get_contents("php://input");   
+    //Envio de datos al manejador
+    $respuesta = $_mozos->delete($postBody);
+    //Respuesta 
+    header('Content-Type: application/json');
+    if(isset($respuesta["result"]["error_id"])){
+        $responseCode = $respuesta["result"]["error_id"];
+        http_response_code($responseCode);
+    }else {
+        http_response_code(200);
+    }
+    echo json_encode($respuesta);
 } else {
     header('Content-Type: application/json');
     $respuesta = $_respuestas->error_405();
