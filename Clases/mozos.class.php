8552da3078abf7965474623df841746207927832
Franco1606 <frankobertolotti@gmail.com>
2022-03-18 14:42:42 -0300

    Agregue los metodos crud

--- Clases/mozos.class.php
+++ Clases/mozos.class.php
@@ -23,10 +23,24 @@ class mozos extends conexion {
         }
     }
 
+    public function obtenerMozo($mozoID) {
+        $query = "SELECT * FROM " . $this->tabla . " WHERE mozoID = '" . $mozoID . "'";        
+        $datosMozos = parent::obtenerDatos($query);
+        if($datosMozos) {
+            return $datosMozos[0];
+        } else {
+            return 0;
+        }
+    }
+
     private function verificarExistencia(){
-        $query = "SELECT mozoID FROM " . $this->tabla . " WHERE usuario = '" . $this->usuario . "'";        
-        $datos = parent::obtenerDatos($query);        
-        return count($datos);
+        $query = "SELECT * FROM " . $this->tabla . " WHERE usuario = '" . $this->usuario . "'";        
+        $datosMozo = parent::obtenerDatos($query);        
+        if($datosMozo) {
+            return $datosMozo[0];
+        } else {
+            return 0;
+        }
     }
 
     public function post($postBody) {
@@ -64,7 +78,7 @@ class mozos extends conexion {
                     }
                 } else {
                     //existe el usuario                
-                    return $_respuestas->error_200("El usuario ". $this->usuario . "ya existe ");
+                    return $_respuestas->error_200("El usuario ". $this->usuario . " ya existe ");
                 }
             }
         }
@@ -78,6 +92,105 @@ class mozos extends conexion {
         $resp = parent::nonQueryId($query);
         if($resp){
              return $resp;
+        }else{
+            return 0;
+        }
+    }
+
+    public function put($postBody) {
+        $_respuestas = new respuestas;
+        $_token = new token;        
+        $datos = json_decode($postBody, true);
+        $verificarToken = $_token->verificarToken($datos);
+        if($verificarToken == 1){
+            if(!isset($datos["mozoID"]) || !isset($datos["usuario"]) || !isset($datos["nombre"]) || !isset($datos["estado"]) || !isset($datos["password"]) || !isset($datos["newPassword"])){
+                return $_respuestas->error_400();
+            } else {
+                $this->mozoID = $datos["mozoID"];
+                $this->usuario = $datos["usuario"];
+                $datosUsuario = $this->verificarExistencia();                
+                if(!($datosUsuario != 0 && $datosUsuario["mozoID"] != $this->mozoID)) {
+                    $verifPass = 1;
+                    if($datos["newPassword"] == null || $datos["newPassword"] == "") {
+                        $this->password = $datos["password"];
+                    } else {
+                        $this->password = $datos["newPassword"];
+                        if(strlen($this->password) < 6 || strlen($this->password) > 16) {
+                            $verifPass = 0;
+                        }
+                    }                    
+                    if($verifPass) {
+                        if($datos["newPassword"] == null || $datos["newPassword"] == "") {
+                            $this->password = $datos["password"];
+                        } else {
+                            $this->password = parent::encriptar($datos["newPassword"]);
+                        }                        
+                        $this->nombre = $datos["nombre"];
+                        $this->estado = $datos["estado"];                
+                        $resp = $this->modificarMozo();                               
+                        if($resp) {                    
+                            $respuesta = $_respuestas->response;
+                            $respuesta["result"] = array(
+                                "status" => "ok",                         
+                                "mozoID" => $this->mozoID
+                            );
+                            return $respuesta;
+                        } else {
+                            return $_respuestas->error_500("Error interno del servidor, el cambio no se guardo o no hubo modificaciones en el registro");
+                        }
+                    } else {
+                        return $_respuestas->error_200("La contraseña debe tener entre 6 y 16 caracteres");
+                    }
+                } else {
+                    //existe el usuario                
+                    return $_respuestas->error_200("El usuario ". $this->usuario . " ya existe ");
+                }
+            }
+
+        } else {
+            return $verificarToken;
+        }
+    }
+
+    private function modificarMozo(){
+        $query = "UPDATE " . $this->tabla . " SET usuario ='" . $this->usuario . "', nombre = '" . $this->nombre . "', estado = '" . $this->estado . "', password = '" . $this->password . "' WHERE mozoID = '" . $this->mozoID . "'";
+        $resp = parent::nonQuery($query);       
+        if($resp >= 1){
+             return $resp;
+        }else{
+            return 0;
+        }
+    }
+    
+    public function delete($postBody){
+        $_respuestas = new respuestas;
+        $_token = new token;        
+        $datos = json_decode($postBody, true);
+        $verificarToken = $_token->verificarToken($datos);
+        if($verificarToken == 1) {
+            if(!isset($datos['mozoID'])){
+                return $_respuestas->error_400();
+            }else{
+                $this->mozoID = $datos['mozoID'];
+                $resp = $this->eliminarMozo();
+                if($resp){                    
+                    $respuesta = $_respuestas->response;
+                    $respuesta["result"] = array(
+                        "mozoID" => $this->mozoID
+                    );                    
+                    return $respuesta;
+                }else{
+                    return $_respuestas->error_500("Error interno del servidor, no se pudo borrar el registro o el registro no existia");
+                }
+            }
+        }
+    }
+
+    private function eliminarMozo(){
+        $query = "DELETE FROM " . $this->tabla . " WHERE mozoID = '" . $this->mozoID . "'";               
+        $resp = parent::nonQuery($query);
+        if($resp >= 1 ){
+            return $resp;
         }else{
             return 0;
         }
