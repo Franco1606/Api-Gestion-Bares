4d06b7a6d1adad069bf3bea931629efccf2bb97c
Franco1606 <frankobertolotti@gmail.com>
2022-03-19 17:05:59 -0300

    Agregue metodo put

--- Clases/sesiones.class.php
+++ Clases/sesiones.class.php
@@ -63,16 +63,57 @@ class sesiones extends conexion {
     }
 
     private function insertarSesion(){
-        $query = "INSERT INTO " . $this->tabla . " (usuarioID, mesaID, solicitadaFecha, estado) values ('" . $this->usuarioID . "','" . $this->mesaID ."','" . $this->fechaActual . "','" . $this->estado . "')";
-        print_r($query);
+        $query = "INSERT INTO " . $this->tabla . " (usuarioID, mesaID, solicitadaFecha, estado) values ('" . $this->usuarioID . "','" . $this->mesaID ."','" . $this->fechaActual . "','" . $this->estado . "')";        
         $this->sesionID = parent::nonQueryId($query);
-        /* $resp = parent::nonQueryId($query);
+        $resp = parent::nonQueryId($query);
         if($resp){
              return $resp;
         }else{
             return 0;
-        } */
-    }    
+        }
+    }
+
+    public function put($postBody) {
+        $_respuestas = new respuestas;
+        $_token = new token;        
+        $datos = json_decode($postBody, true);
+        $verificarToken = $_token->verificarToken($datos);
+        if($verificarToken == 1){
+            if(!isset($datos["estado"]) || !isset($datos["sesionID"])){
+                return $_respuestas->error_400();
+            } else {
+                $this->estado = $datos["estado"];
+                $this->sesionID = $datos["sesionID"];
+                if($this->estado == "cerrada") {
+                    $resp = $this->cambiarCerrada();
+                } 
+                if($resp) {                    
+                    $respuesta = $_respuestas->response;
+                    $respuesta["result"] = array(
+                        "status" => "ok",
+                        "ordenID" => $this->sesionID
+                    );
+                    return $respuesta;
+                } else {
+                    return $_respuestas->error_500("Error interno del servidor, el cambio no se guardo o no hubo modificaciones en el registro");
+                }
+            }
+
+        } else {
+            return $verificarToken;
+        }
+    }
+
+    private function cambiarCerrada(){
+        $fechaActual = date("Y-m-d H:i:s");
+        $query = "UPDATE " . $this->tabla . " SET estado ='" . $this->estado . "', cerradaFecha = '" . $fechaActual . "' WHERE sesionID = '" . $this->sesionID . "'";         
+        $resp = parent::nonQuery($query);       
+        if($resp >= 1){
+             return $resp;
+        }else{
+            return 0;
+        }
+    }
 }
 
 ?>
