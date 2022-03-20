d76ca9af2710035c4603887b56f8258f5c715ab4
Franco1606 <frankobertolotti@gmail.com>
2022-03-20 19:50:08 -0300

    Agregue metodos para llamar mozo

--- Clases/sesiones.class.php
+++ Clases/sesiones.class.php
@@ -37,34 +37,59 @@ class sesiones extends conexion {
         }
     }
 
-    //No necesita Token porque el cliente puede solicitar una mesa e inicia una sesion (INSERT en sesiones)
+    public function verificarSesionAbierta() {
+        $query = "SELECT * FROM " . $this->tabla . " WHERE usuarioID = '" . $this->usuarioID . "' AND mesaID = '" .$this->mesaID. "' AND (estado = 'abierta' OR estado = 'solicitada')";
+        $datosSesion = parent::obtenerDatos($query);
+        if($datosSesion) {
+            return $datosSesion[0];
+        } else {
+            return 0;
+        }
+    }
+    
     public function post($postBody) {
-        $_respuestas = new respuestas;
-        $_token = new token;
-        $datos = json_decode($postBody, true);               
-        if(!isset($datos['usuarioID']) || !isset($datos['mesaID']) || !isset($datos['estado'])){
-            return $_respuestas->error_400();
-        }else{
+        $datos = json_decode($postBody, true);
+        if(isset($datos["usuarioID"])) {
             $this->usuarioID = $datos['usuarioID'];
-            $this->mesaID = $datos['mesaID'];
-            $this->fechaActual = date("Y-m-d H:i:s");
-            $this->estado = $datos['estado'];            
-            $resp = $this->insertarSesion();
-            if($resp){                
-                $respuesta = $_respuestas->response;
-                $respuesta["result"] = array(
-                    "productoID" => $resp
-                );
-                return $respuesta;
-            }else{
-                return $_respuestas->error_500();
+        }
+        if(isset($datos["mesaID"])) {
+            $this->mesaID = $datos['mesaID'];            
+        }
+        $this->fechaActual = date("Y-m-d H:i:s");
+
+        if(isset($datos["llamarMozo"])) {            
+            $datosSesion = $this->verificarSesionAbierta();                        
+            if($datosSesion) {
+                $this->sesionID = $datosSesion["sesionID"];                
+                $this->llamarMozo();
+            } else {
+                $this->estado = "solicitada";
+                $this->sesionID = $this->insertarSesion();
+                $this->llamarMozo();
             }
-        }       
+        } else {
+            $_respuestas = new respuestas;
+            $_token = new token;
+            if(!isset($datos['usuarioID']) || !isset($datos['mesaID']) || !isset($datos['estado'])){
+                return $_respuestas->error_400();
+            }else{                
+                $this->estado = $datos['estado'];            
+                $resp = $this->insertarSesion();
+                if($resp){                
+                    $respuesta = $_respuestas->response;
+                    $respuesta["result"] = array(
+                        "sesionID" => $resp
+                    );
+                    return $respuesta;
+                }else{
+                    return $_respuestas->error_500();
+                }
+            }
+        }
     }
 
     private function insertarSesion(){
         $query = "INSERT INTO " . $this->tabla . " (usuarioID, mesaID, solicitadaFecha, estado) values ('" . $this->usuarioID . "','" . $this->mesaID ."','" . $this->fechaActual . "','" . $this->estado . "')";        
-        $this->sesionID = parent::nonQueryId($query);
         $resp = parent::nonQueryId($query);
         if($resp){
              return $resp;
@@ -73,12 +98,22 @@ class sesiones extends conexion {
         }
     }
 
+    private function llamarMozo(){
+        $query = "UPDATE " . $this->tabla . " SET llamarMozo = 1 WHERE sesionID = '" . $this->sesionID . "'";                
+        $resp = parent::nonQuery($query);       
+        if($resp >= 1){
+             return $resp;
+        }else{
+            return 0;
+        }
+    }
+
     public function put($postBody) {
+        $datos = json_decode($postBody, true); 
         $_respuestas = new respuestas;
-        $_token = new token;        
-        $datos = json_decode($postBody, true);
+        $_token = new token;
         $verificarToken = $_token->verificarToken($datos);
-        if($verificarToken == 1){
+            if($verificarToken == 1){
             if(!isset($datos["estado"]) || !isset($datos["sesionID"])){
                 return $_respuestas->error_400();
             } else {
@@ -86,7 +121,7 @@ class sesiones extends conexion {
                 $this->sesionID = $datos["sesionID"];
                 if($this->estado == "cerrada") {
                     $resp = $this->cambiarCerrada();
-                } 
+                }    
                 if($resp) {                    
                     $respuesta = $_respuestas->response;
                     $respuesta["result"] = array(
@@ -98,10 +133,11 @@ class sesiones extends conexion {
                     return $_respuestas->error_500("Error interno del servidor, el cambio no se guardo o no hubo modificaciones en el registro");
                 }
             }
-
+    
         } else {
             return $verificarToken;
-        }
+        }       
+        
     }
 
     private function cambiarCerrada(){
@@ -113,7 +149,7 @@ class sesiones extends conexion {
         }else{
             return 0;
         }
-    }
+    }    
 }
 
 ?>
