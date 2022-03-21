cb7f5e710cca5db692b302acb53a88615b4eeb30
Franco1606 <frankobertolotti@gmail.com>
2022-03-21 20:12:46 -0300

    Agregue metodos para el get

--- pedidos.php
+++ pedidos.php
@@ -6,12 +6,23 @@ $_pedidos = new pedidos;
 $_respuestas = new respuestas;
 
 if($_SERVER["REQUEST_METHOD"] == "GET") {
-    if(isset($_GET["ordenID"])) {
+    if(isset($_GET["usuarioID"]) && isset($_GET["cocina"])) {
+        $usuarioID = $_GET["usuarioID"];
+        $cocina = $_GET["cocina"];
+        $datosPedidos = $_pedidos->obtenerPedidosEnCocina($usuarioID, $cocina);
+    } else if(isset($_GET["ordenID"])) {
         $ordenID = $_GET["ordenID"];        
         $datosPedidos = $_pedidos->obtenerPedidos($ordenID);
     } else if(isset($_GET["sesionID"])) {
         $sesionID = $_GET["sesionID"];        
         $datosPedidos = $_pedidos->obtenerPedidosPorSesion($sesionID);
+    } else if(isset($_GET["usuarioID"])) {        
+        $usuarioID = $_GET["usuarioID"];        
+        $datosPedidos = $_pedidos->obtenerPedidosPorUsuario($usuarioID);
+    } else if(isset($_GET["productoID"]) && isset($_GET["cocina"])) {
+        $productoID = $_GET["productoID"];
+        $cocina = $_GET["cocina"];
+        $datosPedidos = $_pedidos->obtenerPedidosPorProductoId($productoID, $cocina);
     } 
     header("Content-Type: application/json");
     echo json_encode($datosPedidos);
